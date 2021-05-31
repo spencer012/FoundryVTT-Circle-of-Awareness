4daaabeea5dec151971cf52a2f9b7b5d49ab43ca
Spencer <43939691+spencer012@users.noreply.github.com>
2021-05-31 13:27:45 -0500

    up

--- scripts/awareness.js
+++ scripts/awareness.js
@@ -1,12 +1,39 @@
 "use strict";
 
 const MODULE_ID = "FoundryVTT-Circle-of-Awareness";
+const MODULE_NAME = "Circle of Awareness";
 
-Hooks.on('renderTokenConfig', (app, html, data) => {
+Hooks.on('renderTokenConfig', "on", (app, html, data) => {
 	console.log(app);
 	console.log(html);
 	console.log(data);
 	console.log("------");
-	const selector = html.find('header:contains(Vision)').nextAll('header')[0];
-	console.log(selector);
+
+	const tokenConfigElement = html[0];
+	const resources = tokenConfigElement.querySelector('.tab[data-tab="vision"]');
+	const isAware = tokenConfig.object.getFlag(MODULE_ID, 'isAware');
+	const isAwareCheckboxElement = htmlToElement(`
+		<div class="form-group">
+			<label>Awareness</label>
+			<input type="checkbox" name="${MODULE_ID}-is-aware" data-dtype="Boolean" ${isAware && 'checked'}>
+		</div>`);
+	resources.append(isAwareCheckboxElement);
+
+	isAwareCheckboxElement.addEventListener('change', (ev) => {
+		const isChecked = ev?.target?.checked || false;
+		return tokenConfig.object.setFlag(MODULE_ID, 'isAware', isChecked);
+	});
+
+	const isAwareRadius = tokenConfig.object.getFlag(MODULE_ID, 'isAwareRadius') || 8;
+	const isAwareRadiusCheckboxElement = htmlToElement(`
+		<div class="form-group">
+			<label>Awareness Radius</label>
+			<input type="number" name="${MODULE_ID}-is-aware" data-dtype="Boolean" value="${isAwareRadius}">
+		</div>`);
+	resources.append(isAwareRadiusCheckboxElement);
+
+	isAwareRadiusCheckboxElement.addEventListener('change', (ev) => {
+		const isChecked = ev?.target?.value || 8;
+		return tokenConfig.object.setFlag(MODULE_ID, 'isAwareRadius', isChecked);
+	});
 });
