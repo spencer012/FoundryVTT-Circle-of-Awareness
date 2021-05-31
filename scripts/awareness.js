6af8bcedd8931f9637f173e3aa163016fc8db0b4
Spencer <43939691+spencer012@users.noreply.github.com>
2021-05-31 10:51:06 -0500

    logging

--- scripts/awareness.js
+++ scripts/awareness.js
@@ -3,7 +3,10 @@
 const MODULE_ID = "FoundryVTT-Circle-of-Awareness";
 
 Hooks.on('renderTokenConfig', (app, html, data) => {
+	console.log(app);
+	console.log(html);
+	console.log(data);
+	console.log("------");
 	const selector = html.find('header:contains(Vision)').nextAll('header')[0];
 	console.log(selector);
-
 });
