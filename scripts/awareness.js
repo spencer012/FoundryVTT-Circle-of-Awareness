35a0d9cfb3ab7917cc8123b9486f7861ec8338e3
Spencer <43939691+spencer012@users.noreply.github.com>
2021-06-03 16:56:16 -0500

    .

--- scripts/awareness.js
+++ scripts/awareness.js
@@ -13,21 +13,21 @@ function htmlToElement(html) {
 function updateAwareness(tokenConfig) {
 	console.log("Trying to update");
 	console.log(tokenConfig);
-	if (tokenConfig.object.getFlag(MODULE_ID, 'isAware')) {
+	if (tokenConfig?.isAware || tokenConfig.object.getFlag(MODULE_ID, 'isAware')) {
 		console.log("active");
 		var awareness = tokenConfig.token._object.awarenessLight;
-		awareness = awareness || new PointSource(tokenConfig.token._object);
+		awareness = awareness || new PointSource(tokenConfig.token._object, "sight");
 		awareness.active = true;
 		awareness.initialize({ 
 			x: 0, 
 			y: 0, 
-			dim: tokenConfig.object.getFlag(MODULE_ID, 'isAwareRadius') || 8, 
+			dim: tokenConfig?.isAwareRadius || tokenConfig.object.getFlag(MODULE_ID, 'isAwareRadius') || 8, 
 			bright: 0, 
 			angle: 0, 
 			rotation: 0, 
 			color: 0, 
 			alpha: 1, 
-			darkness: { min: 0, max: 0 }, 
+			darkness: { min: 0, max: 1 }, 
 			type: CONST.SOURCE_TYPES.LOCAL, 
 			animation: null, 
 			seed: 0 
@@ -54,6 +54,7 @@ Hooks.on('renderTokenConfig', (tokenConfig, html, data) => {
 	isAwareCheckboxElement.addEventListener('change', (ev) => {
 		const isChecked = ev?.target?.checked || false;
 		tokenConfig.object.setFlag(MODULE_ID, 'isAware', isChecked);
+		tokenConfig.isAware = isChecked;
 		updateAwareness(tokenConfig);
 	});
 
@@ -68,12 +69,8 @@ Hooks.on('renderTokenConfig', (tokenConfig, html, data) => {
 	isAwareRadiusElement.addEventListener('change', (ev) => {
 		const value = ev?.target?.value || 8;
 		tokenConfig.object.setFlag(MODULE_ID, 'isAwareRadius', value);
+		tokenConfig.isAwareRadius = value;
 		updateAwareness(tokenConfig);
 	});
-});
-
-Hooks.on('updateTokenConfig', (tokenConfig, html, data) => {
-	console.log("UPDATE");
-	console.log(tokenConfig);
 });
 
