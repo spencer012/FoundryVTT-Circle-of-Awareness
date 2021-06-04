5621a4100d553b1cecd01882deaa2987edda16a0
Spencer <43939691+spencer012@users.noreply.github.com>
2021-06-04 07:44:00 -0500

    .

--- scripts/awareness.js
+++ scripts/awareness.js
@@ -18,19 +18,19 @@ function updateAwareness(tokenConfig) {
 		var awareness = tokenConfig.token._object.awarenessLight;
 		awareness = awareness || new PointSource(tokenConfig.token._object, "sight");
 		awareness.active = true;
-		awareness.initialize({ 
-			x: 0, 
-			y: 0, 
-			dim: tokenConfig?.isAwareRadius || tokenConfig.object.getFlag(MODULE_ID, 'isAwareRadius') || 8, 
-			bright: 0, 
-			angle: 0, 
-			rotation: 0, 
-			color: 0, 
-			alpha: 1, 
-			darkness: { min: 0, max: 1 }, 
-			type: CONST.SOURCE_TYPES.LOCAL, 
-			animation: null, 
-			seed: 0 
+		awareness.initialize({
+			x: 0,
+			y: 0,
+			dim: tokenConfig?.isAwareRadius || tokenConfig.object.getFlag(MODULE_ID, 'isAwareRadius') || 8,
+			bright: 0,
+			angle: 0,
+			rotation: 0,
+			color: 0,
+			alpha: 1,
+			darkness: { min: 0, max: 1 },
+			type: CONST.SOURCE_TYPES.LOCAL,
+			animation: null,
+			seed: 0
 		});
 	} else {
 		console.log("unactive");
@@ -74,3 +74,82 @@ Hooks.on('renderTokenConfig', (tokenConfig, html, data) => {
 	});
 });
 
+Hooks.on("init", () => {
+	Token.prototype.updateSource = function updateSource({ defer = false, deleted = false, noUpdateFog = false } = {}) {
+		if (CONFIG.debug.sight) {
+			SightLayer._performance = { start: performance.now(), tests: 0, rays: 0 }
+		}
+
+		// Prepare some common data
+		const origin = this.getSightOrigin();
+		const sourceId = this.sourceId;
+		const d = canvas.dimensions;
+
+		// Update light source
+		const isLightSource = this.emitsLight && !this.data.hidden;
+		if (isLightSource && !deleted) {
+			const bright = Math.min(this.getLightRadius(this.data.brightLight), d.maxR);
+			const dim = Math.min(this.getLightRadius(this.data.dimLight), d.maxR);
+			this.light.initialize({
+				x: origin.x,
+				y: origin.y,
+				dim: dim,
+				bright: bright,
+				angle: this.data.lightAngle,
+				rotation: this.data.rotation,
+				color: this.data.lightColor,
+				alpha: this.data.lightAlpha,
+				animation: this.data.lightAnimation
+			});
+			canvas.lighting.sources.set(sourceId, this.light);
+			if (!defer) {
+				this.light.drawLight();
+				this.light.drawColor();
+			}
+		}
+		else {
+			canvas.lighting.sources.delete(sourceId);
+			if (isLightSource && !defer) canvas.lighting.refresh();
+		}
+
+		// Update vision source
+		const isVisionSource = this._isVisionSource();
+		if (isVisionSource && !deleted) {
+			let dim = Math.min(this.getLightRadius(this.data.dimSight), d.maxR);
+			const bright = Math.min(this.getLightRadius(this.data.brightSight), d.maxR);
+			this.vision.initialize({
+				x: origin.x,
+				y: origin.y,
+				dim: dim,
+				bright: bright,
+				angle: this.data.sightAngle,
+				rotation: this.data.rotation
+			});
+			canvas.sight.sources.set(sourceId, this.vision);
+			if (!defer) {
+				this.vision.drawLight();
+				canvas.sight.refresh({ noUpdateFog });
+			}
+
+			if (this.getFlag(MODULE_ID, 'isAware')) {
+				let dim = Math.min(this.getLightRadius(this.getFlag(MODULE_ID, 'isAwareRadius')), d.maxR);
+				this.vision.initialize({
+					x: origin.x,
+					y: origin.y,
+					dim: dim,
+					angle: 360
+				});
+				canvas.sight.sources.set(sourceId, this.vision);
+
+				if (!defer) {
+					this.vision.drawLight();
+					canvas.sight.refresh({ noUpdateFog });
+				}
+			}
+		}
+		else {
+			canvas.sight.sources.delete(sourceId);
+			if (isVisionSource && !defer) canvas.sight.refresh();
+		}
+	}
+});
